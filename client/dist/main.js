7b4b8ff89e3256baafded6209f77d52ff9febc2e
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-05-04 15:46:44 +0700

    change to arrow function

--- client/dist/main.js
+++ client/dist/main.js
@@ -1,11 +1,11 @@
 const ws = new WebSocket("ws://localhost:5487/ws");
 
-ws.onopen = function () {
+ws.onopen = () => {
     console.log("Connected to server");
 };
 
-ws.onmessage = function (evt) {
-    var { name, artist, album, image } = JSON.parse(evt.data);
+ws.onmessage = (event) => {
+    let { name, artist, album, image } = JSON.parse(event.data);
 
     if (name) {
         document.getElementById("box").style.visibility = "visible";
